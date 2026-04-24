943c63538bdfb71c3d7af7cf7142cee243eb16bb
Rayhan <ehhannazmi@gmail.com>
2026-04-24 09:22:34 +0700

    Upload GitHub pertamaku

---
+++ data.js
@@ -0,0 +1,11 @@
+const databaseProyek = {
+    "sistem_pos": {
+        judul: "Sistem POS untuk PT ABC",
+        keahlian: ["Javascript" , "PHP"],
+        gambar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHSFU-4uIN9HgSWBnKEkZmODs4BjH3049spw&s",
+        latar: "Ini adalah proyek magang dimana saya membuat sistem POS untuk PT ABC",
+        tantangan: "Kebutuhan dari PT-nya cukup rumit",
+        pelajaran: "Saya mendapatkan pelajaran bahwa sistem POS PT ABC ideal untuk skala kecil",
+        link: ""
+    },
+};
